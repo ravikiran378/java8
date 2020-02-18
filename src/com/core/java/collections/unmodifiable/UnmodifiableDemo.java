81fa2cebba21efa4128b06f324b14df6cd38805d
Moguluri <rmoguluri@apple.com>
2020-02-18 15:36:09 +0530

    Unmodifiable list example

---
+++ src/com/core/java/collections/unmodifiable/UnmodifiableDemo.java
@@ -0,0 +1,27 @@
+package com.core.java.collections.unmodifiable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UnmodifiableDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> a = new ArrayList<>();
+		
+		a.add("1");
+		a.add("2");
+		a.add("3");
+		
+		List<String> unmodifiableList1 = Collections.unmodifiableList(new ArrayList<>(a));
+		List<String> unmodifiableList2 = Collections.unmodifiableList(a);
+		
+		a.add("4");
+		
+		System.out.println(unmodifiableList1);
+		System.out.println(unmodifiableList2);
+		
+	}
+
+}
